04ed223c9fcf69ac77318e9e8db611e5f09849d4
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-21 12:36:05 +0530

    add: component structure of app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, {useState, useEffect} from "react";
-import {
-  MenuItem, 
-  FormControl,
-  Select,
-} from "@mui/material";
+import {MenuItem, FormControl, Select, Card, CardContent} from "@mui/material";
 import './App.css';
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 
 function App() {
   const [country, setCountry] = useState("worldwide");
@@ -31,17 +29,35 @@ function App() {
   }
   return (
     <div className="app">
-    <div className="app_header">
-    <h1>Covid 19 Tracker app</h1>
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" onChange={onCountryChange} value={country}>
-          <MenuItem value="worldwide">WorldWide</MenuItem>
-          {countries.map((country)=>{
-            return <MenuItem value={country.value}>{country.name}</MenuItem>
-          })}
-        </Select>
-      </FormControl>
+
+    <div className="app__left">
+      <div className="app__header">
+      <h1>Covid 19 Tracker app</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country)=>{
+              return <MenuItem value={country.value}>{country.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={100} total={200} />
+        <InfoBox title="Recovered" cases={100} total={200}/>
+        <InfoBox title="Deaths" cases={100} total={200} />
+      </div>
+      <Map />
     </div>
+
+
+    <Card className="app__right">
+        <CardContent>
+            <h3>Live Cases By Country</h3>
+            <h3> WorldWide New Cases</h3>
+        </CardContent>
+    </Card>
+    
     </div>
   );
 }
---
+++ src/InfoBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@mui/material';
+function InfoBox({title, cases, total}) {
+  return (
+    <Card className="infoBox">
+        <CardContent>
+            <Typography className="infoBox__title" color="textSecondary">
+                {title}
+            </Typography>
+            <h2 className="infoBox__cases">{cases}</h2>
+            <Typography className="infoBox__total" color="textSecondary">
+                {total} Total
+            </Typography>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default InfoBox;
